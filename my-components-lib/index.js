94a5d9efe1360710b666aa515925057df72f78cb
Balaj Maqbool <balajmaqbool54@gmail.com>
2025-07-13 22:11:31 +0500

    untracked unecessary files

--- my-components-lib/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Button } from "./src/components/Button.jsx";
-export { default as Card } from "./src/components/Card.jsx";
-export { default as Modal } from "./src/components/Modal.jsx";
